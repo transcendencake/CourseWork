46677d21c19590c064a0b33ab4ba5fe58843df1d
pav.ziaz@gmail.com <pav.ziaz@gmail.com>
2021-02-28 19:06:37 +0300

    Добавьте файлы проекта.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CourseWork.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CourseWork.Areas.Identity.IdentityHostingStartup))]
+namespace CourseWork.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Controllers/CookiesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseWork.Controllers
+{
+    public class CookiesController : Controller
+    {
+        [HttpPost]
+        public IActionResult SetTheme(string returnUrl, string theme)
+        {
+            HttpContext.Response.Cookies.Delete("theme");
+            HttpContext.Response.Cookies.Append("theme", theme);
+
+            return Redirect(returnUrl);
+        }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string returnUrl, string language)
+        {
+            HttpContext.Response.Cookies.Delete("language");
+            HttpContext.Response.Cookies.Append("language", language);
+
+            return Redirect(returnUrl);
+        }
+    }
+}
